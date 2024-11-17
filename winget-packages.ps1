f0b2895358a56b209f11beabe8a10d508a33fe13
Itokonoue Kaoru <TheSilentQuill@proton.me>
2024-11-17 21:34:05 +0100

    Create winget-packages.ps1

---
+++ winget-packages.ps1
@@ -0,0 +1,36 @@
+winget install -e --id 7zip.7zip
+winget install -e --id Bitwarden.Bitwarden
+winget install -e --id CodeSector.TeraCopy
+winget install -e --id CrystalDewWorld.CrystalDiskInfo.ShizukuEdition
+winget install -e --id Git.Git
+winget install -e --id Google.AndroidStudio
+winget install -e --id HTTPToolKit.HTTPToolKit
+winget install -e --id IrfanSkiljan.IrfanView
+winget install -e --id IrfanSkiljan.IrfanView.PlugIns
+winget install -e --id IVPN.IVPN
+winget install -e --id Levitsky.FontBase
+winget install -e --id MediaArea.MediaInfo
+winget install -e --id Microsoft.PowerToys
+winget install -e --id Microsoft.VisualStudioCode --override '/SILENT /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders"'
+winget install -e --id MoritzBunkus.MKVToolNix
+winget install -e --id Mozilla.Firefox
+winget install -e --id Mozilla.Thunderbird
+winget install -e --id MullvadVPN.MullvadVPN
+winget install -e --id NordSecurity.NordVPN
+winget install -e --id Postman.Postman
+winget install -e --id PeterPawlowski.foobar2000
+winget install -e --id Rclone.Rclone
+winget install -e --id RustDesk.RustDesk
+winget install -e --id ShareX.ShareX
+winget install -e --id SpecialK.SpecialK
+winget install -e --id Spotify.Spotify
+winget install -e --id Valve.Steam
+winget install -e --id Vencord.Vesktop
+winget install -e --id WinMerge.WinMerge
+winget install -e --id WerWolv.ImHex
+winget install -e --id aria2.aria2
+winget install -e --id jqlang.jq
+winget install -e --id voidtools.Everything.Alpha
+winget install -e --skip-dependencies --id yt-dlp.yt-dlp
+winget install -e -i --id MediaArea.MediaInfo.GUI
+winget install -e -i --id Python.Python.3.13
