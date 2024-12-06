01211f8aec0d6e17784cf4e304d3215c02b276a4
Itokonoue Kaoru <TheSilentQuill@proton.me>
2024-12-06 01:57:20 +0100

    Complete rework for better stability

--- winget-select-packages.ps1
+++ winget-select-packages.ps1
@@ -1,104 +1,157 @@
 $scriptLogic = @'
 $availableApps = @(
-    "7zip.7zip",
-    "Bitwarden.Bitwarden",
-    "CodeSector.TeraCopy",
-    "CrystalDewWorld.CrystalDiskInfo.ShizukuEdition",
-    "File-New-Project.EarTrumpet",
-    "Git.Git",
-    "Google.AndroidStudio",
-    "HTTPToolKit.HTTPToolKit",
-    "IrfanSkiljan.IrfanView",
-    "IrfanSkiljan.IrfanView.PlugIns",
-    "IVPN.IVPN",
-    "JanDeDobbeleer.OhMyPosh",
-    "Levitsky.FontBase",
+    "TheBrowserCompany.Arc",
+    "Brave.Brave",
+    "Google.Chrome",
+    "Mozilla.Firefox",
+    "Mozilla.Firefox.DeveloperEdition",
     "LibreWolf.LibreWolf",
-    "Logitech.GHUB",
-    "MediaArea.MediaInfo",
-    "MediaArea.MediaInfo.GUI",
+    "MullvadVPN.MullvadBrowser",
+    "TorProject.TorBrowser",
+    "Betterbird.Betterbird",
+    "OpenWhisperSystems.Signal",
+    "Vencord.Vesktop",
+    "Google.AndroidStudio",
+    "Kitware.CMake",
+    "Git.Git",
+    "HTTPToolKit.HTTPToolKit",
+    "WerWolv.ImHex",
+    "JanDeDobbeleer.OhMyPosh",
+    "Postman.Postman",
+    "Python.Python.3.12",
+    "Microsoft.VisualStudioCode",
+    "VSCodium.VSCodium",
+    "Valve.Steam",
     "Microsoft.PowerShell",
     "Microsoft.PowerToys",
-    "Microsoft.VisualStudioCode",
-    "Microsoft.WindowsTerminal",
+    "ImageMagick.ImageMagick",
+    "IrfanSkiljan.IrfanView",
+    "IrfanSkiljan.IrfanView.PlugIns",
+    "MediaArea.MediaInfo",
+    "MediaArea.MediaInfo.GUI",
     "MoritzBunkus.MKVToolNix",
-    "Mozilla.Firefox",
-    "Mozilla.Thunderbird",
+    "FlorianHeidenreich.Mp3tag",
+    "OBSProject.OBSStudio",
+    "Spotify.Spotify",
+    "yt-dlp.yt-dlp",
+    "IVPN.IVPN",
     "MullvadVPN.MullvadVPN",
     "NordSecurity.NordVPN",
-    "Postman.Postman",
-    "PeterPawlowski.foobar2000",
-    "Python.Python.3.13",
-    "Rclone.Rclone",
     "RustDesk.RustDesk",
+    "AgileBits.1Password",
+    "7zip.7zip",
+    "aria2.aria2",
+    "AutoHotkey.AutoHotkey",
+    "Bitwarden.Bitwarden",
+    "CrystalDewWorld.CrystalDiskInfo",
+    "File-New-Project.EarTrumpet",
+    "voidtools.Everything.Alpha",
+    "Levitsky.FontBase",
+    "junegunn.fzf",
+    "Logitech.GHUB",
+    "jqlang.jq",
+    "LocalSend.LocalSend",
+    "Rclone.Rclone",
     "ShareX.ShareX",
     "SpecialK.SpecialK",
-    "Spotify.Spotify",
-    "Valve.Steam",
-    "Vencord.Vesktop",
+    "Ookla.Speedtest.CLI",
+    "CodeSector.TeraCopy",
     "WinMerge.WinMerge",
-    "WerWolv.ImHex",
-    "ajeetdsouza.zoxide",
-    "aria2.aria2",
-    "jqlang.jq",
-    "junegunn.fzf",
-    "voidtools.Everything.Alpha",
-    "yt-dlp.yt-dlp"
+    "ajeetdsouza.zoxide"
 )
 
-$cursorPosition = 0
-$selectedApps = New-Object System.Collections.Generic.List[System.String]
+function Show-CheckboxMenu {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string[]]$Arguments,
 
-function Show-Menu {
-    Clear-Host
-    Write-Host "Use the Up/Down arrows to select, Space to toggle, and Enter to confirm your choices"
-    Write-Host ""
+        [Parameter(Position = 1)]
+        [string]$Title = "Select options",
 
-    for ($i = 0; $i -lt $availableApps.Length; $i++) {
-        $checkboxSelection = if ($selectedApps.Contains($availableApps[$i])) { "[X]" } else { "[ ]" }
-        if ($i -eq $cursorPosition) {
-            Write-Host "$checkboxSelection`t> $($availableApps[$i])" -ForegroundColor Cyan
+        [Parameter(Position = 2)]
+        [int]$PageSize = 15
+    )
+
+    $selectedApps = @{}
+    $currentIndex = 0
+    $startIndex = 0
+
+    function Show-DrawMenu {
+        Clear-Host
+        Write-Host $Title -ForegroundColor Cyan
+        Write-Host
+
+        $endIndex = [math]::Min($startIndex + $PageSize, $Arguments.Count)
+
+        for ($i = $startIndex; $i -lt $endIndex; $i++) {
+            if ($selectedApps.ContainsKey($i)) {
+                $prefix = "[X]"
+            } else {
+                $prefix = "[ ]"
+            }
+
+            if ($i -eq $currentIndex) {
+                Write-Host "$prefix $($Arguments[$i])" -BackgroundColor DarkBlue -ForegroundColor White
+            } else {
+                Write-Host "$prefix $($Arguments[$i])"
+            }
         }
-        else {
-            Write-Host "$checkboxSelection`t  $($availableApps[$i])"
+
+        Write-Host
+        Write-Host "Navigate with ↑ and ↓, select/unselect with Space, confirm with Enter." -ForegroundColor Yellow
+        Write-Host "Showing $($startIndex + 1)-$endIndex of $($Arguments.Count)" -ForegroundColor Gray
+    }
+
+    Show-DrawMenu
+
+    while ($true) {
+        $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
+
+        switch ($key) {
+            38 {
+                if ($currentIndex -gt 0) {
+                    $currentIndex--
+                    if ($currentIndex -lt $startIndex) {
+                        $startIndex = [math]::Max($startIndex - 1, 0)
+                    }
+                    Show-DrawMenu
+                }
+            }
+
+            40 {
+                if ($currentIndex -lt ($Arguments.Count - 1)) {
+                    $currentIndex++
+                    if ($currentIndex -ge ($startIndex + $PageSize)) {
+                        $startIndex = [math]::Min($startIndex + 1, $Arguments.Count - $PageSize)
+                    }
+                    Show-DrawMenu
+                }
+            }
+
+            32 {
+                if ($selectedApps.ContainsKey($currentIndex)) {
+                    $selectedApps.Remove($currentIndex)
+                } else {
+                    $selectedApps[$currentIndex] = $true
+                }
+                Show-DrawMenu
+            }
+
+            13 {
+                $sortedApps = $selectedApps.Keys | Sort-Object
+                return $sortedApps | ForEach-Object { $Arguments[$_] }
+            }
         }
     }
 }
 
-Show-Menu
-
-while ($pressedKey -ne 'Enter') {
-    $pressedKey = [System.Console]::ReadKey($true).Key
-
-    switch ($pressedKey) {
-        "UpArrow" {
-            $cursorPosition = if ($cursorPosition -gt 0) { $cursorPosition - 1 } else { $availableApps.Length - 1 }
-        }
-        "DownArrow" {
-            $cursorPosition = if ($cursorPosition -lt $availableApps.Length - 1) { $cursorPosition + 1 } else { 0 }
-        }
-        "Spacebar" {
-            $currentApp = $availableApps[$cursorPosition]
-            if ($selectedApps.Contains($currentApp)) {
-                $selectedApps.Remove($currentApp)
-            }
-            else {
-                $selectedApps.Add($currentApp)
-            }
-        }
-        "Enter" {
-            if ($selectedApps.Count -gt 0) {
-                break
-            }
-        }
-    }
-
-    Show-Menu
-}
+$selectedApps = Show-CheckboxMenu -Arguments $availableApps -Title "Select Your Options" -PageSize 15
 
 Clear-Host
-
 Write-Host "Installing selected applications..."
+Write-Host
+
 foreach ($selectedApp in $selectedApps) {
     $installCommand = "winget install -e --id $selectedApp"
 
@@ -117,8 +170,25 @@ foreach ($selectedApp in $selectedApps) {
 }
 
 Write-Host "`nInstallation of $($selectedApps.Count) $(if ($selectedApps.Count -eq 1) { 'app' } else { 'apps' }) complete."
-Start-Sleep -Seconds 0.75
+Write-Host "Press any key to exit or wait 10 seconds..."
+
+$startTime = Get-Date
+
+while ((Get-Date) -lt $startTime.AddSeconds(10)) {
+    if ([console]::KeyAvailable) {
+        # Key was pressed
+        [void][console]::ReadKey($true)
+        Write-Host "Key pressed. Exiting..."
+        break
+    }
+    Start-Sleep -Milliseconds 100
+}
+
+if ((Get-Date) -ge $startTime.AddSeconds(10)) {
+    Write-Host "Timeout reached. Exiting..."
+}
+# Start-Sleep -Seconds 0.75
 '@
 
-$encodedScriptLogic = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($scriptLogic))
-Start-Process WT.exe -ArgumentList "powershell.exe -NoLogo -NoProfile -EncodedCommand $encodedScriptLogic"
+$scriptLogic = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($scriptLogic))
+Start-Process WT.exe -ArgumentList "powershell.exe -NoLogo -NoProfile -EncodedCommand $scriptLogic"
